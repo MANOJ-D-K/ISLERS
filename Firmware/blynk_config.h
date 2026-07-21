65d93c6083bd4c1b6b341cdd97b3900e9cd246df
MANOJ DK <manomanojkumar005@gmail.com>
2026-07-21 18:36:35 +0530

    Create blynk_config.h

---
+++ Firmware/blynk_config.h
@@ -0,0 +1,14 @@
+#ifndef BLYNK_CONFIG_H
+#define BLYNK_CONFIG_H
+
+#define BLYNK_TEMPLATE_ID "ID"
+#define BLYNK_TEMPLATE_NAME "NAME"
+#define BLYNK_AUTH_TOKEN "TOKEN"
+
+#include <WiFi.h>
+#include <BlynkSimpleEsp32.h>
+
+char ssid[] = "WIFI NAME";
+char pass[] = "PASSWORD";
+
+#endif
